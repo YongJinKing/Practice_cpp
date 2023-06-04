676e491edba3b05044a8a4534f293305451a2875
YongJinKing <dydwls120@gmail.com>
2023-06-05 00:48:31 +0900

    Hello_World

---
+++ print_helloworld_cpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void)
+{
+    int num = 20;
+    std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"Hello "<<"World!"<<std::endl;
+    std::cout<<num<<' '<<'B'<<' '<< num+3 << std::endl;
+}
