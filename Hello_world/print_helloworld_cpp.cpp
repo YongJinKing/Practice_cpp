bf8604bf24b649293bd93519fe6ab8d0de8d3216
YongJinKing <dydwls120@gmail.com>
2023-06-05 01:32:36 +0900

    Hello_plz

--- Hello_world/print_helloworld_cpp.cpp
+++ Hello_world/print_helloworld_cpp.cpp
@@ -6,5 +6,5 @@ int main(void)
     std::cout<<"Hello World!"<<std::endl;
     std::cout<<"Hello "<<"World!"<<std::endl;
     std::cout<<num<<' '<<'B'<<' '<< num+3 << std::endl;
-    std::cout<<"test"<<std::endl;
+    std::cout<<"test"<<std::endl;//plz
 }
