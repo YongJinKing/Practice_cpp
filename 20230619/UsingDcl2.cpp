ff597f58941b16c9ac2ac429dcb97cc2d3400e24
YongJinKing <dydwls120@gmail.com>
2023-06-19 22:48:55 +0900

    20230619

---
+++ 20230619/UsingDcl2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+  using std::cin;
+  using std::cout;
+  using std::endl;
+int main() {
+  int num1 = 0;
+  cout<<"put the num : ";
+  cin >> num1;
+  cout << "num : "<< num1 << endl;
+}
