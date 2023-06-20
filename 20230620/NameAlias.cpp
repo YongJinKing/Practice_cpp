b71bd2e36ec38f9322b6433eef1ad40862147cd7
YongJinKing <dydwls120@gmail.com>
2023-06-20 22:55:13 +0900

    20230620

---
+++ 20230620/NameAlias.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+namespace AAA
+{
+  namespace BBB
+  {
+    namespace CCC
+    {
+      int num1;
+      int num2;
+    }
+  }
+
+}
+
+
+
+int main() {
+  AAA::BBB::CCC::num1 = 10;
+  AAA::BBB::CCC::num2 = 20;
+  namespace ABC = AAA::BBB::CCC;
+  cout<<ABC::num1<<endl;
+  cout<<ABC::num2<<endl;
+}
